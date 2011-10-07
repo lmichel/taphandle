8f1f9fc0d33d885c8e36d44fd593a1c5ea293f1e
laurent.mistahl <laurent.mistahl@a650fa1c-b235-2144-ab7c-4a05cc6e1c4a>
2011-10-07 12:21:11 +0000

    Released for the Pune IVOA meeting

---
+++ WebContent/javascript/resultPaneControler.js
@@ -0,0 +1,62 @@
+jQuery.extend({
+
+	ResultPaneController: function(model, view){
+		/**
+		 * listen to the view
+		 */
+		var vlist = {
+				controlShowRecord: function(oid){
+					model.processShowRecord(oid);
+				},
+				controlShowMeta: function(){
+					model.processShowMeta();
+				},
+				controlShowMetaNode: function(treepath){
+					model.processShowMetaNode(treepath);
+				},
+				controlDownloadVOTable: function(){
+					model.downloadVOTable();				
+				},
+				controlDownloadFITS: function(){
+					model.downloadFITS();				
+				},
+				controlDownloadZip: function(){
+					model.downloadZip();				
+				},
+				controlSampBroadcast: function(){
+					model.sampBroadcast();				
+				},
+				controlSetTreePath : function(treepath){
+					model.setTreePath(treepath);
+				}
+
+		}
+		view.addListener(vlist);
+
+		var mlist = {
+				jobInProgress : function(){
+					view.showProgressStatus();
+				},
+				jobFailed : function(textStatus){
+					view.showFailure(textStatus);
+				},
+				jobIsDone : function(dataJSONObject){
+					view.displayResult(dataJSONObject);
+				},
+				tableIsInit : function(dataJSONObject, query){
+					view.initTable(dataJSONObject, query);
+				},
+				detailIsLoaded: function(oid, dataJSONObject){
+					view.showDetail(oid, dataJSONObject);
+				},
+				metaIsLoaded: function(dataJSONObject){
+					view.showMeta(dataJSONObject);
+				},
+				counterpartsAreLoaded: function(dataJSONObject){
+					view.showCounterparts(dataJSONObject);
+				}
+		}
+
+		model.addListener(mlist);
+	}
+});
