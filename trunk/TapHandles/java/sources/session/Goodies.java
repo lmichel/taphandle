a7eb4407b539b9202ae880679917022a5f001104
laurent.mistahl@gmail.com <laurent.mistahl@gmail.com@a650fa1c-b235-2144-ab7c-4a05cc6e1c4a>
2013-09-09 18:12:45 +0000

    Mise en place du upoad (suite)

--- trunk/TapHandles/java/sources/session/Goodies.java
+++ trunk/TapHandles/java/sources/session/Goodies.java
@@ -1,11 +1,16 @@
 package session;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import resources.RootClass;
 import translator.GoodiesIngestor;
@@ -40,11 +45,11 @@ public  class Goodies extends RootClass{
 		String dirName =  this.baseDirectory + WEB_USER_GOODIES_LIST +File.separator + nodeKey;
 		validWorkingDirectory(dirName );
 		FileUtils.copyFile(
-				  new File(jobPath +File.separator  + VOTABLE_JOB_RESULT)
+				new File(jobPath +File.separator  + VOTABLE_JOB_RESULT)
 				, new File(dirName +File.separator  + ((goodiesName.endsWith(".xml"))? goodiesName: goodiesName + ".xml") )
-				);
+		);
 	}
-	
+
 	/**
 	 * Returns the full pathname of to file goodiesName stored into the goodies position list.
 	 * The returned path must match a non existing file. If the file already exists, it is appended
@@ -66,27 +71,59 @@ public  class Goodies extends RootClass{
 		} 
 		return fileName;
 	}
-	
+
 	/**
 	 * @param item
 	 * @throws Exception 
 	 */
-	public String ingestUserList(FileItem item, double radius) throws Exception{
+	public String ingestUserList(FileItem item) throws Exception{
 		File f = new File(this.getNewUserListPath(item.getName()));
-		item.write(f);		
-		GoodiesIngestor gi = new GoodiesIngestor(this.baseDirectory + WEB_USER_GOODIES_LIST, item.getName(), radius);
+		item.write(f);	
+		return processUserList( item.getName());
+	}
+
+	/**
+	 * Public for debug purpose can be run without reference to FileItem
+	 * @param listName
+	 * @return
+	 * @throws Exception
+	 */
+	public String processUserList(String listName)  throws Exception{
+		GoodiesIngestor gi = new GoodiesIngestor(this.baseDirectory + WEB_USER_GOODIES_LIST, listName);
 		gi.ingestUserList();
-		return gi.getReport();
+		return gi.getReport();		
+	}
+	
+	public void dropUserList(String name) throws Exception{
+		JSONObject report = this.getJsonContent(WEB_USER_GOODIES_LIST, name + ".json");
+		File f = new File(this.baseDirectory + WEB_USER_GOODIES_LIST + File.separator + report.get("nameOrg")) ;
+		if( f.exists() ) {
+			logger.info("Remove file " + f.getAbsolutePath());
+			f.delete();
+		}
+		f = new File(this.baseDirectory + WEB_USER_GOODIES_LIST + File.separator + report.get("nameVot")) ;
+		if( f.exists() ) {
+			logger.info("Remove file " + f.getAbsolutePath());
+			f.delete();
+		}
+		f = new File(this.baseDirectory + WEB_USER_GOODIES_LIST + File.separator + report.get("nameReport")) ;
+		if( f.exists() ) {
+			logger.info("Remove file " + f.getAbsolutePath());
+			f.delete();
+		}
 	}
 	/**
 	 * Return a JSON description of the Goodies content
-	 * Retuned JSON is [{nodekey:"..", tables: ["...", "..."]}, ....] 
+	 * Retuned JSON is [{nodekey:"..", posLists: [{"positions", "date", "nameOrg", "nameVot", "nameReport}, ....] 
 	 * @return
+	 * @throws Exception 
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
 	 */
 	@SuppressWarnings("unchecked")
-	public JSONArray getJsonContent() {
+	public JSONArray getJsonContent() throws Exception {
 		JSONArray retour = new JSONArray();
-		
+
 		String[] nodes = (new File(this.baseDirectory)).list();
 		for( String node: nodes) {
 			String ndir = this.baseDirectory + File.separator + node;
@@ -95,14 +132,55 @@ public  class Goodies extends RootClass{
 				jnode.put("nodekey", node);
 				JSONArray jtables = new JSONArray();
 				String[] tables = (new File(ndir)).list();
-				for( String table: tables){
-					jtables.add(table);
+				for( String table: tables){					
+					if( table.endsWith(".json") ){
+						JSONObject jslist  = this.getJsonContent(node, table);
+						if ( jslist  != null ){
+							jtables.add(jslist);
+						}
+					}
 				}
-				jnode.put("tables", jtables);
+				jnode.put("posLists", jtables);
 				retour.add(jnode);
 			}
 		}		
 		return retour;
 	}
 
+	/**
+	 * Returns the JSON description contained in the file table from the node WEB_USER_GOODIES_LIST
+	 * Returned JSON is {"positions", "date", "nameOrg", "nameVot", "nameReport}
+	 * @param table
+	 * @return
+	 * @throws Exception
+	 */
+	public JSONObject getUserListReport(String table) throws Exception {
+		return getJsonContent(WEB_USER_GOODIES_LIST, table);
+	}
+
+	/**
+	 * Returns the JSON description contained in the file table from the node table
+	 * Returned JSON is {"positions", "date", "nameOrg", "nameVot", "nameReport}
+	 * @param node
+	 * @param table
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	private JSONObject getJsonContent(String node, String table) throws Exception {
+		String ndir = this.baseDirectory + File.separator + node;
+		JSONObject jslist  = new JSONObject();
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(new FileReader(ndir + File.separator + table));
+		JSONObject jsonObject = (JSONObject) obj;
+		for( String k: new String[]{"nameReport", "nameVot", "nameOrg", "positions", "date"}){
+			Object str;
+			if( (str = jsonObject.get(k)) == null ) {
+				return null;
+			} else {
+				jslist.put(k, str);
+			}
+		}
+		return jslist;
+	}
 }
