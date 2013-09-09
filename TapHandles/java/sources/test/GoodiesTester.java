89deb32f20c85d0ec761068a6a7824896fa767ec
laurent.mistahl@gmail.com <laurent.mistahl@gmail.com@a650fa1c-b235-2144-ab7c-4a05cc6e1c4a>
2013-09-09 18:13:17 +0000

    Mise en place du upoad (suite)

--- TapHandles/java/sources/test/GoodiesTester.java
+++ TapHandles/java/sources/test/GoodiesTester.java
@@ -2,7 +2,11 @@ package test;
 
 import java.io.IOException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import resources.RootClass;
+import session.Goodies;
 import translator.GoodiesIngestor;
 
 
@@ -12,9 +16,21 @@ public class GoodiesTester extends RootClass {
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
-		GoodiesIngestor gi = new GoodiesIngestor("/home/michel/Desktop", "list.pos", 3);
-		gi.ingestUserList();
+	public static void main(String[] args) throws Exception {
+		String base = "/Users/laurentmichel/Desktop/goodies/";
+		
+		Goodies g = new Goodies(base);
+		g.processUserList("list.pos");		
+		JSONArray jso;
+		jso = g.getJsonContent();
+		System.out.println(jso.toJSONString());
+		g.processUserList("list2.pos");		
+		jso = g.getJsonContent();
+		System.out.println(jso.toJSONString());
+		g.dropUserList("list.pos");
+		jso = g.getJsonContent();
+		System.out.println(jso.toJSONString());
+
 	}
 
 }
