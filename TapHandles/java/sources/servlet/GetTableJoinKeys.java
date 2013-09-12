081b5d319baca3bec8f7bdb3e36b1222c1036ca0
laurent.mistahl <laurent.mistahl@a650fa1c-b235-2144-ab7c-4a05cc6e1c4a>
2013-09-12 06:38:55 +0000

    Suport both "node"  and "schema" as parmeter name

--- TapHandles/java/sources/servlet/GetTableJoinKeys.java
+++ TapHandles/java/sources/servlet/GetTableJoinKeys.java
@@ -24,8 +24,13 @@ public class GetTableJoinKeys extends RootServlet implements Servlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		printAccess(request, true);
 		response.setContentType("application/json; charset=UTF-8");
-
+		/*
+		 * The node concept is specific to taphandle/saada whereas the concept of schema is specific to TAP, then we support both
+		 */
 		String node = this.getParameter(request, "node");
+		if( node == null ){
+			node = this.getParameter(request, "schema");
+		}
 		String table = this.getParameter(request, "table");
 		if( node == null || node.length() ==  0 ) {
 			reportJsonError(request, response, "gettableatt: no node specified");
