000e00ebb032f37156a9c64a2aab1022c5fa484c
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2016-07-06 11:52:08 +0200

    synchro

--- taphandle/java/sources/test/TapNodeTest.java
+++ taphandle/java/sources/test/TapNodeTest.java
@@ -31,16 +31,6 @@ public class TapNodeTest  extends RootClass {
 		try {
 		rm = new RegistryMark("tapnodetest", "ivo://tapnodetest", args[0], "test", false, true);
 		tn = new TapNode(rm, "/tmp/meta");
-<<<<<<< HEAD:TapHandles/java/sources/test/TapNodeTest.java
-		//tn.buildJsonTableAttributes("viz2.III/205/catalog");
-		//tn.buildJsonTableAttributes("III/205/catalog");
-=======
-//		tn.buildJsonTableDescription("PhotoObjDR7");
-//		tn.buildJsonTableAttributes("PhotoObjDR7");
-////		tn.buildJsonTableAttributes("III/205/catalog");
-//		tn.buildJsonTableAttributes("\"I/306A/types\"");
-		tn.buildJsonTableDescription("viz2.III/205/catalog");
->>>>>>> 0d85a640c7393334381c177f4623783e3aeca709:taphandle/java/sources/test/TapNodeTest.java
 		} catch(Exception e) {
 			e.printStackTrace(System.out);
 		} finally {
