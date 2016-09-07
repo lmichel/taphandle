a7298bf0026bbb9b5d5d5b4e88da34140f1ef182
Laurent Michel <laurent.michel@astro.unistra.fr>
2016-09-07 17:01:10 +0200

    Test programs

--- taphandle/java/sources/test/TapGAVO.java
+++ taphandle/java/sources/test/TapGAVO.java
@@ -17,15 +17,15 @@ import resources.RootClass;
  * @author michel
  *
  */
-public class TapVizier  extends RootClass {
+public class TapGAVO  extends RootClass {
 	private static final String baseDir = System.getProperty("java.io.tmpdir") ;
-	private static final String baseUrlN = "http://tapvizier.u-strasbg.fr/beta/TAPVizieR/tap";
+	private static final String baseUrlN = "http://dc.zah.uni-heidelberg.de/__system__/tap/run/tap/";
 
 	public static void main(String[] args) throws Exception {
 		validWorkingDirectory(baseDir + "/nodebase");
 
 		
-		RegistryMark rm = new RegistryMark("vizier", " ", baseUrlN, "test", false, true);
+		RegistryMark rm = new RegistryMark("GAVO", " ", baseUrlN, "test", false, true);
 		TapNode tn = new TapNode(rm, "/tmp/meta");
 
 		JSONParser parser = new JSONParser();
@@ -35,21 +35,34 @@ public class TapVizier  extends RootClass {
 		int gen = 0;
 		for(Object sn: schemas) {
 			JSONObject s = (JSONObject)sn;
-			System.out.println("*********** " + s.get("name"));
-			JSONArray tables = (JSONArray) s.get("tables");
-			int cpt = 0;
-			for( Object ts: tables) {
-				JSONObject t = (JSONObject)ts;
-				cpt++;
-				gen++;
-				if( cpt < 5 || cpt >= 5) {
-					System.out.println("    " + cpt + "/" + gen + " " + t.get("name"));
-					DataTreePath dataTreePath = new DataTreePath(s.get("name").toString(), (String)t.get("name"), "");
-					tn.buildJsonTableAttributes(dataTreePath);
-					tn.buildJsonTableDescription(dataTreePath);
-					System.exit(1);
+
+			if( ((String)(s.get("name"))).equals("dmubin") ) {
+				JSONArray tables = (JSONArray) s.get("tables");
+				for( Object ts: tables) {
+					JSONObject t = (JSONObject)ts;
+					if( ((String)(t.get("name"))).endsWith("main") ) {
+						System.out.println("found");
+						DataTreePath dataTreePath = new DataTreePath(s.get("name").toString(), (String)t.get("name"), "");
+						tn.buildJsonTableAttributes(dataTreePath);
+					}
 				}
 			}
+//			JSONObject s = (JSONObject)sn;
+//			System.out.println("*********** " + s.get("name"));
+//			JSONArray tables = (JSONArray) s.get("tables");
+//			int cpt = 0;
+//			for( Object ts: tables) {
+//				JSONObject t = (JSONObject)ts;
+//				cpt++;
+//				gen++;
+//				if( cpt < 5 || cpt >= 5) {
+//					System.out.println("    " + cpt + "/" + gen + " " + t.get("name"));
+//					DataTreePath dataTreePath = new DataTreePath(s.get("name").toString(), (String)t.get("name"), "");
+//					tn.buildJsonTableAttributes(dataTreePath);
+//					tn.buildJsonTableDescription(dataTreePath);
+//					System.exit(1);
+//				}
+//			}
 		}	
 		System.out.println(tn.filterTableList(2).toJSONString());
 	}
