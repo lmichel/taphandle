902ab57254e1df99e628ba8ecd5d8d3e7cb50dc0
Laurent Michel <laurent.michel@astro.unistra.fr>
2016-09-15 14:43:43 +0200

    version

--- taphandle/java/sources/resources/RootClass.java
+++ taphandle/java/sources/resources/RootClass.java
@@ -34,7 +34,7 @@ import tapaccess.TapException;
  *
  */
 public class RootClass {
-	public static final String VERSION = "2.1";
+	public static final String VERSION = "2.1.1";
 	/**
 	 * Logger used everywhere in the project
 	 */
