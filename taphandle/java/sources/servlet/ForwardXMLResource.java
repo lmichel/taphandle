e535745fa4db51c0d4c75c15dc0e8f0395eecad0
Laurent Michel <laurent.michel@astro.unistra.fr>
2016-11-24 14:16:32 +0100

    Anonymous https support

--- taphandle/java/sources/servlet/ForwardXMLResource.java
+++ taphandle/java/sources/servlet/ForwardXMLResource.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -42,28 +43,33 @@ public class ForwardXMLResource extends RootServlet {
 		this.printAccess(request, true);
 		try {
 			String target = request.getParameter("target");
-			URL url = new URL(target);
-			URLConnection conn = url.openConnection();
-			if( conn.getContentType() != null && (conn.getContentType().startsWith("text/xml") || conn.getContentType().startsWith("application/x-votable+xml"))) {
-			logger.info("Forward URL " + target + " of type " +  conn.getContentType());
-				OutputStream out = response.getOutputStream();
-				bis = new BufferedInputStream(conn.getInputStream());
-				response.setContentType("text/xml");
-				response.setHeader("Content-Length" , conn.getHeaderField("Content-Length"));
-
-				byte[] buffer = new byte[10000];
-				int lt=0 , l;
-				while ( (l = bis.read(buffer)) != -1 ) {
-					lt += l;
-					if( lt > MAX_LENGTH ) {
-						throw new Exception("Forwarding XML message longer than " + MAX_LENGTH  + " is not allowed" );
-					}
-					out.write(buffer, 0, l);
+			if( target.startsWith("http:")) {
+				URL url = new URL(target);
+				URLConnection conn = url.openConnection();
+				if( conn.getContentType() != null && (conn.getContentType().startsWith("text/xml") || conn.getContentType().startsWith("application/x-votable+xml"))) {
+					logger.info("Forward URL " + target + " of type " +  conn.getContentType());
+					this.forwardContent(conn, response);
+				} else {
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+							"Request of type " + conn.getContentType() + " cannot be forwarded"  );
 				}
-				out.flush();
+			} else 	if( target.startsWith("https:")) {
+				URL url = new URL(target);
+				HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+				if( conn.getContentType() != null && (conn.getContentType().startsWith("text/xml") || conn.getContentType().startsWith("application/x-votable+xml"))) {
+					logger.info("Forward secure URL " + target + " of type " +  conn.getContentType());
+					this.forwardContent(conn, response);
+				} else {
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+							"Secure request of type " + conn.getContentType() + " cannot be forwarded"  );
+				}
+			} else if( target.startsWith("https:")) {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Cannot forward secure URL  " + target);
 			} else {
-				throw new Exception("Request of type " + conn.getContentType() + " cannot be forwarded"  );
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+						target + " is not a valid URL: can't be forwarded"  );
 			}
+
 		} catch (Exception e2) {
 			e2.printStackTrace();
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST,
@@ -74,4 +80,28 @@ public class ForwardXMLResource extends RootServlet {
 
 	}
 
+	/**
+	 * Forward the content returned by conn to response. Nothing checked here
+	 * @param conn : URL connection (http or https)
+	 * @param response: servlet response
+	 * @throws IOException
+	 */
+	private final void forwardContent(URLConnection conn, HttpServletResponse response) throws IOException{
+		OutputStream out = response.getOutputStream();
+		BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+		response.setContentType("text/xml");
+		response.setHeader("Content-Length" , conn.getHeaderField("Content-Length"));
+
+		byte[] buffer = new byte[10000];
+		int lt=0 , l;
+		while ( (l = bis.read(buffer)) != -1 ) {
+			lt += l;
+			if( lt > MAX_LENGTH ) {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Forwarding XML message longer than " + MAX_LENGTH  + " is not allowed" );
+				return;
+			}
+			out.write(buffer, 0, l);
+		}
+		out.flush();
+	}
 }
