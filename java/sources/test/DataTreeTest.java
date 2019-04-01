aa9b2bb1d81f23d10b225367e687c9bf538417e9
lmichel <laurent.michel@astro.unistra.fr>
2019-04-01 15:01:41 +0200

    test program

---
+++ java/sources/test/DataTreeTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import metabase.DataTreePath;
+
+public class DataTreeTest {
+
+	public static void main(String[] args) throws Exception {
+		DataTreePath dataTreePath = new DataTreePath("EPIC", "EPIC_SPECTRUM", "");
+		System.out.println(dataTreePath);
+
+	}
+
+}
